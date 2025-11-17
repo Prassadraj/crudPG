55c287d4d2aff0355856cdf1e222956184135264
Prassadraj <prassadpr10@gmail.com>
2025-11-18 01:43:56 +0530

    asasas

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -13,7 +13,7 @@ function page() {
   const [popUp, setPopUp] = useState("");
   const [filteredData, setFilteredData] = useState<Customer | null>(null);
   const [customerId, setCustomerId] = useState("");
-  const [editCustomer, setEditCustomer] = useState<Customer | null>({
+  const [createCustomer, setCreateCustomer] = useState<Customer | null>({
     id: "",
     name: "",
     email: "",
@@ -64,6 +64,16 @@ function page() {
       alert(error);
     }
   };
+  const handleCreate = async () => {
+    try {
+      await axios.post("/api/admin", createCustomer);
+      const res = await axios.get("/api/admin");
+      setData(res.data.data);
+      setPopUp("");
+    } catch (error) {
+      alert(error);
+    }
+  };
 
   // get the customer data
   useEffect(() => {
@@ -73,6 +83,12 @@ function page() {
   return (
     <div className="h-full w-full flex flex-col justify-start items-center pt-10 gap-2 md:gap-6">
       <div className="md:text-3xl ">Welcome Admin,</div>
+      <div
+        className="fixed z-20 top-20 right-20 cursor-pointer"
+        onClick={() => setPopUp("create")}
+      >
+        Create Customer
+      </div>
       <div>
         <table className="min-w-full border text-xs md:text-base">
           <thead className="">
@@ -226,6 +242,72 @@ function page() {
                 onClick={handleEdit}
               >
                 Done
+              </div>
+            </div>
+          </div>
+        )}
+        {/* create  */}
+        {popUp == "create" && (
+          <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
+            <div className="bg-gray-800 p-6 rounded shadow-md w-80 flex gap-2 flex-col">
+              <h1 className="text-center">Edit Info</h1>
+              <label htmlFor="">Name</label>
+              <input
+                value={createCustomer?.name || ""}
+                type="text"
+                onChange={(e) =>
+                  setCreateCustomer((prev) =>
+                    prev ? { ...prev, name: e.target.value } : null
+                  )
+                }
+                className="bg-white text-black p-2"
+              />
+              <label htmlFor="">Email</label>
+              <input
+                type="text"
+                value={createCustomer?.email}
+                onChange={(e) =>
+                  setCreateCustomer((prev) =>
+                    prev ? { ...prev, email: e.target.value } : null
+                  )
+                }
+                className="bg-white text-black p-2"
+              />
+              <label htmlFor="">Password</label>
+              <input
+                type="text"
+                value={createCustomer?.password}
+                onChange={(e) =>
+                  setCreateCustomer((prev) =>
+                    prev ? { ...prev, password: e.target.value } : null
+                  )
+                }
+                className="bg-white text-black p-2"
+              />
+              <div className="flex gap-2 items-center">
+                <label htmlFor="" id="check">
+                  Active
+                </label>
+                <input
+                  type="checkbox"
+                  id="active"
+                  onChange={() =>
+                    setCreateCustomer((prev) =>
+                      prev
+                        ? { ...prev, isDeleted: prev.isDeleted === 0 ? 1 : 0 }
+                        : null
+                    )
+                  }
+                  checked={createCustomer?.isDeleted === 0} // checked if Active
+                  name="check"
+                  className="w-4 h-4"
+                />
+              </div>
+              <div
+                className="text-center bg-green-500 p-2 text-black cursor-pointer"
+                onClick={handleCreate}
+              >
+                Create
               </div>
             </div>
           </div>
