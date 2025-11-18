9c0ee4b9298b51b89bce71e1c0bad733e321c060
Prassadraj <prassadpr10@gmail.com>
2025-11-18 14:41:35 +0530

    kjkjk

--- types/validator.ts
+++ types/validator.ts
@@ -37,13 +37,6 @@ type LayoutConfig<Route extends LayoutRoutes = LayoutRoutes> = {
 
 
 // Validate ../../app/page.tsx
-{
-  type __IsExpected<Specific extends AppPageConfig<"/">> = Specific
-  const handler = {} as typeof import("../../app/page.tsx")
-  type __Check = __IsExpected<typeof handler>
-  // @ts-ignore
-  type __Unused = __Check
-}
 
 
 
@@ -52,10 +45,3 @@ type LayoutConfig<Route extends LayoutRoutes = LayoutRoutes> = {
 
 
 // Validate ../../app/layout.tsx
-{
-  type __IsExpected<Specific extends LayoutConfig<"/">> = Specific
-  const handler = {} as typeof import("../../app/layout.tsx")
-  type __Check = __IsExpected<typeof handler>
-  // @ts-ignore
-  type __Unused = __Check
-}
